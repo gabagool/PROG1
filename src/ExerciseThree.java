98c870f110d513e2156d83d54105804da1ded6ec
Björn Moderatho Winther <152904095+ModerathoWinther@users.noreply.github.com>
2023-12-04 18:36:10 +0100

    Add files via upload

---
+++ src/ExerciseThree.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.time.*;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ExerciseThree {
+    public removeAllValuesOf(ArrayList<Object> objects){
+        objects.clear();
+    }
+    public int sumEvenIndices(ArrayList<Integer> ints){
+
+        int sum = 0;
+        return sum;
+    }
+
+    public ArrayList<LocalDate> addDate(ArrayList<LocalDate> list, LocalDate date){
+
+        //OM (datum == date) lägg ej till;
+        return list;
+    }
+    public static ArrayList<String> getLongWords(String a, String b){
+        ArrayList<String> list = new ArrayList<String>();
+        // kod här din knasboll !!
+        return list;
+    }
+    public static String createGreeting(String name){
+        return "Hello " + name + "!";
+    }
+    public static void main(String[] args) {
+
+    }
+}
